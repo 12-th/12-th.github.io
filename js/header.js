a1e3a7525aaaef754deae4c822c011e5bf975935
mq <1358730414@qq.com>
2020-04-24 23:02:47 +0800

    Site updated: 2020-04-24 23:02:46

--- js/header.js
+++ js/header.js
@@ -1,15 +1,8 @@
-function open_sidebar() {
-  $('#mask').fadeIn(500);
-  $('#sidebar').animate({left: '0'}, 500);
-}
-
-function close_sidebar() {
-  let sidebar = $('#sidebar');
-  $('#mask').fadeOut(500);
-  sidebar.animate({left: -sidebar.width()}, 500);
-}
-
-$(function() {
-  $('#btn-sidebar').on('click', open_sidebar);
-  $('#mask').on('click', close_sidebar);
-})
+$(function(){
+    let strs = $('#site-strings').text().split(',');
+    if(strs.length>0)
+    {
+        let index = Math.round(Math.random()*(strs.length-1));;
+        $('#site-description').text(strs[index]);
+    }
+});
